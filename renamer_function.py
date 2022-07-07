b9d19f02331c9e8f6f849801616b6ef06709c3ee
Gabriel Dufurrena <97625699+gabedufurrena@users.noreply.github.com>
2022-07-07 12:45:04 -0400

    Update renamer_function.py

--- renamer_function.py
+++ renamer_function.py
@@ -1,11 +1,10 @@
 def renamer_bot(file_name):
-    file_name = file_name.lower().replace('.', '_').replace('(', '_').replace(')', '_').replace(' ', '_')
+    specialchars = '.,!@#$%^&*() -=+<>?/\|[]{}'
+    for i in specialchars:
+        file_name = file_name.replace(i, '_')
     file_name = file_name.replace('_xlxs', '.xlxs')
     return file_name
 
-# name = 'Example.bad (file).xlxs'
-# print(renamer_bot(name))
-
-fname = input("What's the file name you want to rewrite? ")
-
-print(renamer_bot(fname))
+while True:
+    name = input("What's the file name you want to rewrite? ")
+    print(renamer_bot(name))
